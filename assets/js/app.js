f4d6d1ebd106f04504929d4d7a2a28490ab53dfe
adham bishr <adhambishr@gmail.com>
2017-12-02 17:51:02 -0800

    cleaning code

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,9 @@
   GAME RULES:
 
   - The game has 2 players, playing in rounds
-  - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-  - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-  - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+  - In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND score
+  - BUT, if the player rolls a 1 or a total of 12, all his ROUND score gets lost. After that, it's the next player's turn
+  - The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
   - The first player to reach 100 points on GLOBAL score wins the game
 
   */
@@ -69,6 +69,7 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
     // Update UI
     document.querySelector("#score-" + activePlayer).textContent =
       scores[activePlayer];
+
     // Check if player won the game
     if (scores[activePlayer] >= 100) {
       document.querySelector("#name-" + activePlayer).textContent = "Winner!";
@@ -102,12 +103,9 @@ function nextPlayer() {
   document.getElementById("current-0").textContent = "0";
   document.getElementById("current-1").textContent = "0";
 
-  //document.querySelector(".player-0-panel").classList.remove("active");
-  //document.querySelector(".player-1-panel").classList.add("active");
   document.querySelector(".player-0-panel").classList.toggle("active");
   document.querySelector(".player-1-panel").classList.toggle("active");
 
-  //document.getElementsByClassName("dice").style.display = "none";
   document.getElementById("diceOne").style.display = "none";
   document.getElementById("diceTwo").style.display = "none";
 }
@@ -118,7 +116,6 @@ function init() {
   roundScore = 0;
   gamePlaying = true;
 
-  //document.getElementsByClassName("dice").style.display = "none";
   document.getElementById("diceOne").style.display = "none";
   document.getElementById("diceTwo").style.display = "none";
 
